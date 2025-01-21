07d393c2ac6f362e06d9dd0cf297474a4456de41
jmlvargas <jmlvargas@gmail.com>
2025-01-20 20:54:18 -0600

    Some language servers disabled

--- init.lua
+++ init.lua
@@ -623,9 +623,9 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
-        csharp_ls = {},
-        bashls = {},
+        -- pyright = {},
+        -- csharp_ls = {},
+        -- bashls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
