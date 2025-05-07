179e240c6bb72b1b9a9228f2f6b9ee87e44bf0d8
jmlvargas <jmlvargas@gmail.com>
2025-05-07 13:33:27 -0600

    Added catppuccin colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,12 +1,23 @@
 return {
-  "rebelot/kanagawa.nvim",
-  enabled = true,
-  config = function()
-    require('kanagawa').setup({
-      compile = true,
-      theme = 'wave', -- wave, lotus, dragon
-      transparent = false,
-    })
-    vim.cmd('colorscheme kanagawa-wave')
-  end
+  {
+    "rebelot/kanagawa.nvim",
+    enabled = false,
+    config = function()
+      require('kanagawa').setup({
+        compile = true,
+        theme = 'wave', -- wave, lotus, dragon
+        transparent = false,
+      })
+      vim.cmd('colorscheme kanagawa-wave')
+    end
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000,
+    enabled = true,
+    config = function ()
+      require("catppuccin").setup({
+      })
+      vim.cmd('colorscheme catppuccin')
+    end
+  },
+
 }
