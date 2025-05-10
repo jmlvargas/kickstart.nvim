31bf94c6e80210919d95982b46108e23b7451c83
jmlvargas <jmlvargas@gmail.com>
2025-05-09 21:22:59 -0600

    File browsing key bindings

--- lua/custom/plugins/files.lua
+++ lua/custom/plugins/files.lua
@@ -10,14 +10,17 @@ return {
     config = function()
       -- vim.keymap.set("n", "<Leader>fb", ":Telescope file_browser<CR>")
       -- open file_browser with the path of the current buffer
-      vim.keymap.set("n", "<Leader>fc", ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-        { desc = "Browse from [c]urrent file path" })
-      vim.keymap.set("n", "<Leader>fw", ":Telescope file_browser<CR>", { desc = "Browse from C[W]D" })
+      vim.keymap.set("n", "<leader>ff",
+        ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+        { desc = "Browse Follow Buffer" })
+
+      vim.keymap.set("n", "<leader>f<space>", ":Telescope file_browser<CR>",
+        { desc = "Browse CWD" })
 
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind file' })
+      vim.keymap.set('n', '<leader>fs', builtin.find_files, { desc = 'Search files' })
 
-      vim.keymap.set('n', '<leader>fs', '<cmd>write<CR>', { desc = '[S]ave current file' })
+      vim.keymap.set('n', '<leader>fw', '<cmd>write<CR>', { desc ='Write file' })
     end
 
   }
